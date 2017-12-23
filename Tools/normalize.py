2795bbcce5b681360b3565c1dcd67a223e5e6939
andredelft <32297879+andredelft@users.noreply.github.com>
2017-12-23 16:49:33 +0100

    Update

--- Tools/normalize.py
+++ Tools/normalize.py
@@ -1,63 +1,78 @@
-from uni_beta_code import beta2uni
-from uni_beta_code import uni2beta
+from Tools.uni_beta_code import beta2uni
+from Tools.uni_beta_code import uni2beta
 import regex
 import unicodedata
 
-#IN PROGRESS
-LATIN = [
-	('i',beta2uni('i')),
-	('o',beta2uni('o')),
-	('p',beta2uni('r')),
-	('u',beta2uni('u')),
-	('v',beta2uni('n')),
-	('x',beta2uni('x')),
-	('A',beta2uni('*a')),
-	('B',beta2uni('*B')),
-	('I',beta2uni('*i')),
-	('K',beta2uni('*k')),
-	('M',beta2uni('*m')),
-	('T',beta2uni('*t')),
-	('X',beta2uni('*x')),
-	('Z',beta2uni('*z'))
-]
+MACRON_BREVE = {
+    '\u1fb8':'\u0391', # GREEK CAPITAL LETTER ALPHA WITH VRACHY
+    '\u1fb9':'\u0391', # GREEK CAPITAL LETTER ALPHA WITH MACRON
+    '\u1fb0':'\u03b1', # GREEK SMALL LETTER ALPHA WITH VRACHY
+    '\u1fb1':'\u03b1', # GREEK SMALL LETTER ALPHA WITH MACRON
+    '\u1fd8':'\u0399', # GREEK CAPITAL LETTER IOTA WITH VRACHY
+    '\u1fd9':'\u0399', # GREEK CAPITAL LETTER IOTA WITH MACRON
+    '\u1fd0':'\u03b9', # GREEK SMALL LETTER IOTA WITH VRACHY
+    '\u1fd1':'\u03b9', # GREEK SMALL LETTER IOTA WITH MACRON
+    '\u1fe8':'\u03a5', # GREEK CAPITAL LETTER UPSILON WITH VRACHY
+    '\u1fe9':'\u03a5', # GREEK CAPITAL LETTER UPSILON WITH MACRON
+    '\u1fe0':'\u03c5', # GREEK SMALL LETTER UPSILON WITH VRACHY
+    '\u1fe1':'\u03c5', # GREEK SMALL LETTER UPSILON WITH MACRON
+    '\u0304':'', # COMBINING MACRON
+    '\u0306':''  # COMBINING BREVE
+}
 
-CYRILLIC = [
-	('А',beta2uni('*A')),
-	('М',beta2uni('*M')),
-	('Ф',beta2uni('*F')),
-	('є',beta2uni('e')),
-	('ї',beta2uni('i+')),
-	('Ѱ',beta2uni('*Y'))
-]
+def macron_breve_filter(text):
+    return text.translate(str.maketrans(MACRON_BREVE))
 
-GREEK_AND_COPTIC = [
-	('ϛ',beta2uni('s'))
-]
+MISC_CHARS = {
+    # LATIN
+    '\u0069':'\u03b9', # LATIN SMALL LETTER I
+    '\u006f':'\u03bf', # LATIN SMALL LETTER O
+    '\u0070':'\u03c1', # LATIN SMALL LETTER P
+    '\u0075':'\u03c5', # LATIN SMALL LETTER U
+    '\u0076':'\u03bd', # LATIN SMALL LETTER V
+    '\u0078':'\u03c7', # LATIN SMALL LETTER X
+    '\u0041':'\u0391', # LATIN CAPITAL LETTER A
+    '\u0042':'\u0392', # LATIN CAPITAL LETTER B
+    '\u0049':'\u0399', # LATIN CAPITAL LETTER I
+    '\u004b':'\u039a', # LATIN CAPITAL LETTER K
+    '\u004d':'\u039c', # LATIN CAPITAL LETTER M
+    '\u0054':'\u03a4', # LATIN CAPITAL LETTER T
+    '\u0058':'\u03a7', # LATIN CAPITAL LETTER X
+    '\u005a':'\u0396', # LATIN CAPITAL LETTER Z
+    
+    # CYRILLIC
+    '\u0410':'\u0391', # CYRILLIC CAPITAL LETTER A
+    '\u041c':'\u039c', # CYRILLIC CAPITAL LETTER EM
+    '\u0424':'\u03a6', # CYRILLIC CAPITAL LETTER EF
+    '\u0454':'\u03b5', # CYRILLIC SMALL LETTER UKRAINIAN IE
+    '\u0457':'\u03ca', # CYRILLIC SMALL LETTER YI
+    '\u0470':'\u03a8', # CYRILLIC CAPITAL LETTER PSI
+    
+    # GREEK_AND_COPTIC
+    '\u03db':'\u03c2', # GREEK SMALL LETTER STIGMA
+    
+    # OTHER
+    '\u05d0':'\u03ba', # HEBREW LETTER ALEF
+    '\u00df':'\u03b2'  # LATIN SMALL LETTER SHARP S
+}
 
-OTHER = [
-	(chr(0x5d0),beta2uni('k')), # Hebrew Aleph
-	('ß',beta2uni('b'))
-]
+def misc_chars_filter(text):
+    return text.translate(str.maketrans(MISC_CHARS))
 
-unicode_blocks = [LATIN,CYRILLIC,GREEK_AND_COPTIC,OTHER]
-
-pattern = []
-for block in unicode_blocks:
-	pattern.append([(regex.compile(old),new) for (old,new) in block])
-
+"""
 def normalize_greek(text):
-	words = []
-	for word in text.split():
-		if ''.join(regex.findall('[A-Za-z]',word)) == word:
-			# Make sure to skip words that are entirely in latin form
-			words.append(word)
-		else:
-			for (repl,new) in pattern[0]:
-				word = repl.sub(new,word)
-			words.append(word)
-	
-		for (i,p) in enumerate(pattern):
-			for (repl,new) in p:
-				word = repl.sub(new,word)
-		
-	return ' '.join(words)
+    words = []
+    for word in text.split():
+        if ''.join(regex.findall('[A-Za-z]',word)) == word:
+            # Make sure to skip words that are entirely in latin form
+            words.append(word)
+        else:
+            for (repl,new) in pattern[0]:
+                word = repl.sub(new,word)
+            words.append(word)
+    
+        for (i,p) in enumerate(pattern):
+            for (repl,new) in p:
+                word = repl.sub(new,word)
+        
+    return ' '.join(words)"""
